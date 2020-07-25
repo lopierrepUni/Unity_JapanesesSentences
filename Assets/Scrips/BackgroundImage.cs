97138bef57ad8c1771f5c9128f396ddafbf45127
Christian Lopierre <lopierrep@gmail.com>
2020-07-24 21:40:25 -0500

    GenerateSpacesWordsDina

--- Assets/Scrips/BackgroundImage.cs
+++ Assets/Scrips/BackgroundImage.cs
@@ -6,13 +6,16 @@ using System.Collections.Generic;
 using System.Data;
 using System.IO;
 using System.Linq;
+using UnityEditor;
 using UnityEngine;
 
 public class BackgroundImage : MonoBehaviour
 {
+    public GameObject wordSpace;
+
 
     private Sprite sprite;
-    SqliteCommand command;
+    private SqliteCommand command;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,15 +25,70 @@ public class BackgroundImage : MonoBehaviour
             string sqlQuery = "SELECT * FROM Image_Sentence";
             command.CommandText = sqlQuery;
             IDataReader reader = command.ExecuteReader();
-            List<ImageSentence> list = new List<ImageSentence>();
+            List<ImageSentence> imageSentenceList = new List<ImageSentence>();
             while (reader.Read())
             {
-                list.Add(new ImageSentence(reader.GetInt32(0), reader.GetInt32(1), reader.GetString(2)));
-
+                imageSentenceList.Add(new ImageSentence(reader.GetInt32(0), reader.GetInt32(1), reader.GetString(2)));
             }
 
-            sprite = Resources.Load<Sprite>("Images/" + list.ElementAt(0).Image_Id);
+            sprite = Resources.Load<Sprite>("SentencesImages/" + imageSentenceList.ElementAt(1).Image_Id);
             this.gameObject.GetComponent<SpriteRenderer>().sprite = sprite;
+
+            string sentence = imageSentenceList.ElementAt(1).Sentence;
+            List<string> words = sentence.Split('*').ToList();
+
+            #region Generate the spaces for the words
+
+            //int sign = Application.platform == RuntimePlatform.Android ? -1 : 1;
+            float y = words.Count <= 5 ? -4 : -3f;
+
+
+            //First line
+            if (words.Count >= 3)
+            {
+                Instantiate(wordSpace, new Vector3(0, y, -1), Quaternion.identity);
+                Instantiate(wordSpace, new Vector3(-3.7f, y, -1), Quaternion.identity);
+                Instantiate(wordSpace, new Vector3(3.7f, y, -1), Quaternion.identity);
+                if (words.Count >= 4)
+                {
+                    Instantiate(wordSpace, new Vector3(-7.4f, y, -1), Quaternion.identity);
+                    if (words.Count >= 5)
+                    {
+                        Instantiate(wordSpace, new Vector3(7.4f, y, -1), Quaternion.identity);
+
+                        y = y - 1.5f;
+                        // Second line
+                        if (words.Count >= 6)
+                        {
+                            Instantiate(wordSpace, new Vector3(0, y, -1), Quaternion.identity);
+                            if (words.Count >= 7)
+                            {
+                                Instantiate(wordSpace, new Vector3(-3.7f, y, -1), Quaternion.identity);
+                                if (words.Count >= 8)
+                                {
+                                    Instantiate(wordSpace, new Vector3(3.7f, y, -1), Quaternion.identity);
+                                }
+                                if (words.Count >= 9)
+                                {
+                                    Instantiate(wordSpace, new Vector3(-7.4f, y, -1), Quaternion.identity);
+                                }
+                                if (words.Count >= 10)
+                                {
+                                    Instantiate(wordSpace, new Vector3(7.4f, y, -1), Quaternion.identity);
+                                }
+                            }
+                        }
+                    }
+                }
+
+
+            }
+            #endregion
+
+
+
+
+
         }
         catch (Exception e)
         {
@@ -52,9 +110,7 @@ public class BackgroundImage : MonoBehaviour
         {
 
             //Where to copy the db to
-            string dbDestination = Path.Combine(Application.persistentDataPath, "data");       
-
-
+            string dbDestination = Path.Combine(Application.persistentDataPath, "data");
             dbDestination = Path.Combine(dbDestination, fileName);
             /*var deleteDir = dbDestination.Replace("\\", "/");
             try
@@ -84,8 +140,8 @@ public class BackgroundImage : MonoBehaviour
             }
             Debug.Log("Loaded db file");
 
-          
-        
+
+
             //Create Directory if it does not exist
             if (!Directory.Exists(Path.GetDirectoryName(dbDestination)))
             {
@@ -94,7 +150,7 @@ public class BackgroundImage : MonoBehaviour
 
             //Copy the data to the persistentDataPath where the database API can freely access the file
             try
-            
+
             {
                 File.WriteAllBytes(dbDestination, result);
                 Debug.Log("Copied db file");
@@ -103,7 +159,7 @@ public class BackgroundImage : MonoBehaviour
             {
                 Debug.LogError("Failed to rewrite DataBase: " + e.Message);
             }
-           
+
             try
             {
                 //Tell the db final location for debugging
