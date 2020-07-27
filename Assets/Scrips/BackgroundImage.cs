213580297de981706ee5398c0665e1ca624a92d7
Christian Lopierre <lopierrep@gmail.com>
2020-07-27 11:14:43 -0500

    Next Button

--- Assets/Scrips/BackgroundImage.cs
+++ Assets/Scrips/BackgroundImage.cs
@@ -1,4 +1,5 @@
-﻿using Assets.Scrips.Clases;
+﻿using Assets.Scrips.Classes;
+using Assets.Scrips.Utilities;
 using Mono.Data.Sqlite;
 using System;
 using System.Collections;
@@ -13,89 +14,41 @@ public class BackgroundImage : MonoBehaviour
 {
     public GameObject wordSpace;
 
-
+    private List<GameObject> wordSpaces = new List<GameObject>();
     private Sprite sprite;
     private SqliteCommand command;
+    private SqliteConnection connection;
+    private ImageSentence imageSentence;
     // Start is called before the first frame update
     void Start()
     {
         try
         {
-            RunDbCode("MainDataBase.s3db");
+
+            connection = Functions.RunDbCode("MainDataBase.s3db");
+            connection.Open();
+            command = connection.CreateCommand();
             string sqlQuery = "SELECT * FROM Image_Sentence";
             command.CommandText = sqlQuery;
             IDataReader reader = command.ExecuteReader();
             List<ImageSentence> imageSentenceList = new List<ImageSentence>();
             while (reader.Read())
             {
-                imageSentenceList.Add(new ImageSentence(reader.GetInt32(0), reader.GetInt32(1), reader.GetString(2)));
+                imageSentence = new ImageSentence(reader.GetInt32(0), reader.GetInt32(1), reader.GetString(2));
+                break;
             }
+            sprite = Resources.Load<Sprite>("SentencesImages/" + imageSentence.Image_Id);
+            gameObject.GetComponent<SpriteRenderer>().sprite = sprite;
 
-            sprite = Resources.Load<Sprite>("SentencesImages/" + imageSentenceList.ElementAt(1).Image_Id);
-            this.gameObject.GetComponent<SpriteRenderer>().sprite = sprite;
-
-            string sentence = imageSentenceList.ElementAt(1).Sentence;
+            string sentence = imageSentence.Sentence;
             List<string> words = sentence.Split('*').ToList();
-
-            #region Generate the spaces for the words
-
-            //int sign = Application.platform == RuntimePlatform.Android ? -1 : 1;
-            float y = words.Count <= 5 ? -4 : -3f;
-
-
-            //First line
-            if (words.Count >= 3)
-            {
-                Instantiate(wordSpace, new Vector3(0, y, -1), Quaternion.identity);
-                Instantiate(wordSpace, new Vector3(-3.7f, y, -1), Quaternion.identity);
-                Instantiate(wordSpace, new Vector3(3.7f, y, -1), Quaternion.identity);
-                if (words.Count >= 4)
-                {
-                    Instantiate(wordSpace, new Vector3(-7.4f, y, -1), Quaternion.identity);
-                    if (words.Count >= 5)
-                    {
-                        Instantiate(wordSpace, new Vector3(7.4f, y, -1), Quaternion.identity);
-
-                        y = y - 1.5f;
-                        // Second line
-                        if (words.Count >= 6)
-                        {
-                            Instantiate(wordSpace, new Vector3(0, y, -1), Quaternion.identity);
-                            if (words.Count >= 7)
-                            {
-                                Instantiate(wordSpace, new Vector3(-3.7f, y, -1), Quaternion.identity);
-                                if (words.Count >= 8)
-                                {
-                                    Instantiate(wordSpace, new Vector3(3.7f, y, -1), Quaternion.identity);
-                                }
-                                if (words.Count >= 9)
-                                {
-                                    Instantiate(wordSpace, new Vector3(-7.4f, y, -1), Quaternion.identity);
-                                }
-                                if (words.Count >= 10)
-                                {
-                                    Instantiate(wordSpace, new Vector3(7.4f, y, -1), Quaternion.identity);
-                                }
-                            }
-                        }
-                    }
-                }
-
-
-            }
-            #endregion
-
-
-
-
-
+            wordSpaces = Functions.GeneretaWordsSpaces(words, wordSpace, wordSpaces);
+            connection.Close();
         }
         catch (Exception e)
         {
             Debug.LogError("Failed: " + e.Message);
         }
-
-
     }
 
 
@@ -104,86 +57,5 @@ public class BackgroundImage : MonoBehaviour
     {
 
     }
-    void RunDbCode(string fileName)
-    {
-        try
-        {
 
-            //Where to copy the db to
-            string dbDestination = Path.Combine(Application.persistentDataPath, "data");
-            dbDestination = Path.Combine(dbDestination, fileName);
-            /*var deleteDir = dbDestination.Replace("\\", "/");
-            try
-            {
-                File.Delete(deleteDir);
-            }
-            catch (Exception e)
-            {
-
-            }*/
-
-            //Where the db file is at
-            string dbStreamingAsset = Path.Combine(Application.streamingAssetsPath, fileName);
-
-            byte[] result;
-
-            //Read the File from streamingAssets. Use WWW for Android
-            if (dbStreamingAsset.Contains("://") || dbStreamingAsset.Contains(":///"))
-            {
-                WWW www = new WWW(dbStreamingAsset);
-
-                result = www.bytes;
-            }
-            else
-            {
-                result = File.ReadAllBytes(dbStreamingAsset);
-            }
-            Debug.Log("Loaded db file");
-
-
-
-            //Create Directory if it does not exist
-            if (!Directory.Exists(Path.GetDirectoryName(dbDestination)))
-            {
-                Directory.CreateDirectory(Path.GetDirectoryName(dbDestination));
-            }
-
-            //Copy the data to the persistentDataPath where the database API can freely access the file
-            try
-
-            {
-                File.WriteAllBytes(dbDestination, result);
-                Debug.Log("Copied db file");
-            }
-            catch (Exception e)
-            {
-                Debug.LogError("Failed to rewrite DataBase: " + e.Message);
-            }
-
-            try
-            {
-                //Tell the db final location for debugging
-                Debug.Log("DB Path: " + dbDestination.Replace("/", "\\"));
-                //Add "URI=file:" to the front of the url beore using it with the Sqlite API
-                dbDestination = "URI=file:" + dbDestination;
-
-                //Now you can do the database operation below
-                //open db connection
-                var connection = new SqliteConnection(dbDestination);
-                connection.Open();
-
-                command = connection.CreateCommand();
-                Debug.Log("Success!");
-            }
-            catch (Exception e)
-            {
-                Debug.LogError("Failed: " + e.Message);
-            }
-        }
-        catch (Exception e)
-        {
-            Debug.LogError("Failed: " + e.Message);
-            throw;
-        }
-    }
 }
---
+++ Assets/Scrips/Utilities/WordCategory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scrips.Utilities
+{
+    public enum WordCategory
+    {
+        #region nouns
+        Person,
+        Object,
+        Place,
+        #endregion
+        Time,
+        Number,
+        Counter,
+        Verb,
+        Adjective,
+        Particle,
+        Adverb,       
+
+    }
+}
